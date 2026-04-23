64d9f2129c5d43a83091427d939e0f0411753324
Monochromasity <148627069+Monochromasity@users.noreply.github.com>
2026-04-23 18:01:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("https://raw.githubusercontent.com/Monochromasity/1.0-demonlist/main/list.json");
-.then(x => x.text());
-.then(y => JSON.parse(y));
+fetch("https://raw.githubusercontent.com/Monochromasity/1.0-demonlist/main/list.json")
+.then(x => x.text())
+.then(y => JSON.parse(y))
 .then(z => console.log(z));
