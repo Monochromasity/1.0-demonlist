856dbbb36f26c7e4f8a5c2086d0e8c691b76fe73
Monochromasity <148627069+Monochromasity@users.noreply.github.com>
2026-04-23 18:13:03 +0100

    rework fetch list

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-fetch("https://raw.githubusercontent.com/Monochromasity/1.0-demonlist/main/list.json")
-.then(x => x.text())
-.then(y => const listtxt = y);
-console.log(listtxt);
-const list = JSON.parse(listtxt);
-console.log(list);
+async function fetchlist() {
+  try {
+    const response = await fetch("https://raw.githubusercontent.com/Monochromasity/1.0-demonlist/main/list.json");
+    const list = await response.json();
+    return list
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const list = fetchlist()
+console.log(list)
