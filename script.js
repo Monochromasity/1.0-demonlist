c6cadd3f936b807c6fb945b05d92f31949cf085d
Monochromasity <148627069+Monochromasity@users.noreply.github.com>
2026-04-24 17:20:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,15 +11,17 @@ async function fetchlist() {
 async function printlist() {
   const list = await fetchlist();
   // console.log(JSON.stringify(list));
+  const htmllist = document.getElementById("list");
   for (i = 0; i < list.length; i++) {
     const level = document.createElement("div");
     level.className = "level";
     const placement = document.createElement("b");
-    const placementtxt = document.createTextNode("#".concat(list[i]));
+    const placementtxt = document.createTextNode("#".concat(i+1));
     placement.appendChild(placementtxt);
     placement.className = "placement";
     level.appendChild(placement);
     console.log(list[i]);
+    htmllist.appendChild(level);
   }
 }
 
