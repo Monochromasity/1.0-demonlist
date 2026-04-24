c66328b65f867c3294b6b57f74cb9be9f71b82c1
Monochromasity <148627069+Monochromasity@users.noreply.github.com>
2026-04-24 17:52:33 +0100

    make js actually work list? maybe???

--- script.js
+++ script.js
@@ -13,13 +13,56 @@ async function printlist() {
   // console.log(JSON.stringify(list));
   const htmllist = document.getElementById("list");
   for (i = 0; i < list.length; i++) {
+    // Level div
     const level = document.createElement("div");
     level.className = "level";
+    // Level placement
     const placement = document.createElement("b");
     const placementtxt = document.createTextNode("#".concat(i+1));
     placement.appendChild(placementtxt);
     placement.className = "placement";
     level.appendChild(placement);
+    // Level thumbnail
+    const thumbnail = document.createElement("img");
+    thumbnail.src = "/thumbnails/".concat(list[i]["level"]).concat(".png");
+    level.appendChild(thumbnail);
+    // Level info
+    const info = document.createElement("div");
+    info.className = "info";
+    // Level name
+    const name = document.createElement("p");
+    const nametxt = document.createTextNode(list[i]["level"]);
+    name.appendChild(nametxt);
+    name.className = "name";
+    // Level details
+    const details = document.createElement("p");
+    details.className = "details";
+    // Level creator
+    const creator = document.createElement("span");
+    const creatortxt = document.createTextNode(list[i]["creator"]);
+    creator.appendChild(creatortxt);
+    creator.className = "user";
+    // Level verifier
+    const verifier = document.createElement("span");
+    const verifiertxt = document.createTextNode(list[i]["verifier"]);
+    verifier.appendChild(verifiertxt);
+    verifier.className = "user";
+    // Level details text
+    const detailstxt1 = document.createTextNode("Published by ");
+    details.appendChild(detailstxt1);
+    details.appendChild(creator);
+    const detailstxt2 = document.createTextNode("Verified by ");
+    details.appendChild(detailstxt2);
+    details.appendChild(verifier);
+    // Level rewards
+    const rewards = document.createElement("p");
+    const rewardstxt = document.createTextNode("Rewards ".concat((list.length)/(500*(i+1))).concat(" points"));
+    rewards.appendChild(rewardstxt);
+    rewards.className = "rewards";
+    info.appendChild(name);
+    info.appendChild(details);
+    info.appendChild(rewards);
+    // Append to website
     console.log(list[i]);
     htmllist.appendChild(level);
   }
