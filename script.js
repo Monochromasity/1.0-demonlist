cabf06724dd8ba279c6988d77eff7c747377e3e2
Monochromasity <148627069+Monochromasity@users.noreply.github.com>
2026-04-25 18:09:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ async function printlist() {
     const level = document.createElement("div");
     level.className = "level";
     level.onclick = function() {
-      ${this}.siblings().className = "leveldetailsdisplay";
+      ${this}.siblings().toggleClass("leveldetailsdisplay");
     }
     // Level placement
     const placement = document.createElement("b");
