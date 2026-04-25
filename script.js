ee2fea07827435a43e2408bc098e7c13c5138705
Monochromasity <148627069+Monochromasity@users.noreply.github.com>
2026-04-25 14:26:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function fetchlist() {
   try {
-    const response = await fetch("https://raw.githubusercontent.com/Monochromasity/1.0-demonlist/refs/heads/main/list.json");
+    const response = await fetch("https://raw.githubusercontent.com/Monochromasity/1.0-demonlist/main/list.json");
     const list = await response.json();
     return list;
   } catch (error) {
