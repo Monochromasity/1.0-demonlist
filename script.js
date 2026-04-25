2ebb353a17f1da28270f9734c3df237ebf3a1767
Monochromasity <148627069+Monochromasity@users.noreply.github.com>
2026-04-25 13:37:23 +0100

    round rewards to 2dp

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ async function printlist() {
     details.appendChild(detailstxt2);
     details.appendChild(verifier);
     // Level rewards
-    const rewardvalue = Math.pow((((-7*Math.sqrt(499))/(list.length-1))*((i+1)-list.length))/7, 2) + 1;
+    const rewardvalue = Math.round((Math.pow((((-7*Math.sqrt(499))/(list.length-1))*((i+1)-list.length))/7, 2) + 1)*100)/100;
     const rewards = document.createElement("p");
     const rewardstxt = document.createTextNode("Rewards ".concat(rewardvalue).concat(" points"));
     rewards.appendChild(rewardstxt);
